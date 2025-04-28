4041ad688b10fa2e6acdb3f9793adf02ae2bd34c
yeongchul <revan2015@naver.com>
2025-04-28 15:56:21 +0900

    [feat] 로그인 구현
    임시 로그인 구현

--- backend/mammoth_order_backend/src/main/java/com/project/mammoth_order_backend/member/MemberController.java
+++ backend/mammoth_order_backend/src/main/java/com/project/mammoth_order_backend/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.project.mammoth_order_backend.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,5 +15,17 @@ public class MemberController {
     public ResponseEntity<String> join(@RequestBody JoinDto joinDto) {
         memberService.join(joinDto);
         return ResponseEntity.ok("회원가입 성공");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        boolean isTrue = memberService.login(loginDto);
+        if (isTrue) {
+            return ResponseEntity.ok("로그인 성공");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)  // 실패 시 401 Unauthorized
+                    .body("아이디 또는 비밀번호가 잘못되었습니다.");
+        }
     }
 }
