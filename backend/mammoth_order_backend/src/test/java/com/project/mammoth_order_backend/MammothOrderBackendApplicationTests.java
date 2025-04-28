00e8d372edeb3e348597bea809c87321d1cfd41c
yeongchul <revan2015@naver.com>
2025-04-28 10:05:35 +0900

    [chore] 스프링 초기 세팅
    프로젝트 생성, 기본적인 의존성 추가, application.properties 기본 설정

---
+++ backend/mammoth_order_backend/src/test/java/com/project/mammoth_order_backend/MammothOrderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mammoth_order_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MammothOrderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
