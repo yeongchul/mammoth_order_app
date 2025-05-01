b0494c3486be1166a30eb5d0fa0c8ed837101d0d
yeongchul <revan2015@naver.com>
2025-05-01 11:07:40 +0900

    [remove] 신메뉴 삭제
    신메뉴 entity, repository 삭제

--- backend/mammoth_order_backend/src/main/java/com/project/mammoth_order_backend/order/repository/NewMenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.mammoth_order_backend.order.repository;
-
-import com.project.mammoth_order_backend.order.entity.NewMenu;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NewMenuRepository extends JpaRepository<NewMenu, Long> {
-}
