2fa8be9790f1eadd690d45896a504f5499909356
yeongchul <revan2015@naver.com>
2025-05-04 22:53:41 +0900

    [feat] 상품 관리 페이지 제작
    전체 상품 조회 페이지 추가, 상품 수정 페이지 추가, 상품 관리 API 추가

--- backend/mammoth_order_backend/src/main/java/com/project/mammoth_order_backend/admin/controller/AdminController.java
+++ backend/mammoth_order_backend/src/main/java/com/project/mammoth_order_backend/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.project.mammoth_order_backend.admin.controller;
 import com.project.mammoth_order_backend.admin.dto.MenuUpdateRequestDto;
 import com.project.mammoth_order_backend.admin.service.AdminService;
 import com.project.mammoth_order_backend.order.dto.MenuResponseDto;
+import com.project.mammoth_order_backend.order.entity.Menu;
 import com.project.mammoth_order_backend.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -19,23 +20,29 @@ public class AdminController {
     // 전체 메뉴 보기
     @GetMapping
     public String menuList(Model model) {
-        MenuResponseDto menus = orderService.getMenu();
+        MenuResponseDto menus = orderService.getAllMenus();
         model.addAttribute("menus", menus);
         return "menu-list";
     }
 
     // 단일 메뉴 보기
+    @GetMapping("/{id}")
+    public String getMenuById(@PathVariable("id") Long menuId, Model model) {
+        Menu menu = orderService.findMenuById(menuId);
+        model.addAttribute("menu", menu);
+        return "menu-edit";
+    }
 
     // 상품 수정
-    @PostMapping("/update/{menuId}")
-    public String updateMenu(@PathVariable Long menuId, @ModelAttribute MenuUpdateRequestDto requestDto) {
+    @PostMapping("/update/{id}")
+    public String updateMenu(@PathVariable("id") Long menuId, @ModelAttribute MenuUpdateRequestDto requestDto) {
         adminService.updateMenu(menuId, requestDto);
-        return "redirect:/menus";
+        return "redirect:/admin/menus";
     }
     
     //상품 삭제
-    @PostMapping("/delete/{menuId}")
-    public String deleteMenu(@PathVariable Long menuId) {
+    @PostMapping("/delete/{id}")
+    public String deleteMenu(@PathVariable("id") Long menuId) {
         adminService.deleteMenu(menuId);
         return "redirect:/admin/menus";
     }
